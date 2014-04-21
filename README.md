429a452653164a5bf6698d7916d0478391bf6690
Miroojin Bakshi <gaglu231094@gmail.com>
2014-04-21 14:57:12 +0530

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+#Implementation of erasure coding
+##CS 254 Project 
+
+###Team Members:
+- Kartikeya Upasani
+- Miroojin Bakshi
+
+###Modules used:
+- ffield.py - A python module for carrying out finite field operations.
+- genericmatrix.py - A module for matrix operations over any domain. (Example: complex, binary)
+
+
---
+++ test.py
@@ -0,0 +1,9 @@
+import fec
+
+f = fec.FEC(4,2)
+data = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+s = f.Encode(data)
+print s
+print f.Decode(s[:2])
+
+
