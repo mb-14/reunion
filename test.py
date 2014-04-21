c27dc9e0e2a104a2e5df24176ffd1c9210ec73f2
Miroojin Bakshi <gaglu231094@gmail.com>
2014-04-21 15:02:33 +0530

    Rename to reunion

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import fec
+import reunion
 
-f = fec.FEC(4,2)
+f = reunion.FEC(4,2)
 data = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 s = f.Encode(data)
 print s
